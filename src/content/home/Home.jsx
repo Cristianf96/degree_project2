90ab0117215b314108e7479747fba08ef2a3ff92
CristianWhitesoft <cristian.badillo@whitesoft.com.au>
2022-05-04 11:11:48 -0500

    firebase

--- src/content/home/Home.jsx
+++ src/content/home/Home.jsx
@@ -1,17 +1,26 @@
 import React from 'react'
+import { collection, getDocs } from "firebase/firestore";
+import db from '../../utils/firebase';
 
 // import { homeStyles } from "./homeStyle";
 
 const Home = () => {
 
     const side = localStorage.getItem('maps') ?? ""
-    
+
     React.useEffect(() => {
-        if(side){
+        if (side) {
             localStorage.removeItem('maps')
             window.location.reload(true);
         }
-    },[side])
+        const textDb = async() =>{
+            const querySnapshot = await getDocs(collection(db, "users"));
+            querySnapshot.forEach((doc) => {
+                console.log(`${doc.id} => ${doc.data().age}, ${doc.data().name}`);
+            });
+        }
+        textDb()
+    }, [side])
 
     return (
         <>
