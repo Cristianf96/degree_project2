018af40fb6c5460cb1eef1e7baaf0d11dd018a20
CristianWhitesoft <cristian.badillo@whitesoft.com.au>
2022-05-21 05:05:46 -0500

    21/05/2022 autocomplite and location

--- src/content/maps/Maps.jsx
+++ src/content/maps/Maps.jsx
@@ -1,26 +1,29 @@
-import React, { useEffect, useMemo, useState } from 'react'
-import { GoogleMap, useLoadScript, Marker, Autocomplete } from '@react-google-maps/api'
+import React, { useEffect, useState } from 'react'
+import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api'
+import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
+import axios from 'axios';
 
-import { Box, SpeedDial, SpeedDialAction, Backdrop, Snackbar, IconButton, Card, TextField, Stack } from '@mui/material';
+import { Box, SpeedDial, SpeedDialAction, Backdrop, Snackbar, IconButton, Card, Stack } from '@mui/material';
 import MuiAlert from '@mui/material/Alert';
-// import AddIcon from '@mui/icons-material/Add';
-import SearchIcon from '@mui/icons-material/Search';
+// import SearchIcon from '@mui/icons-material/Search';
 import TipsAndUpdatesIcon from '@mui/icons-material/TipsAndUpdates';
 import PersonIcon from '@mui/icons-material/Person';
 import ForumIcon from '@mui/icons-material/Forum';
 import LogoutIcon from '@mui/icons-material/Logout';
 import SpeedDialIcon from '@mui/material/SpeedDialIcon';
 import MyLocationIcon from '@mui/icons-material/MyLocation';
+// import NavigationIcon from '@mui/icons-material/Navigation';
 
-import DialogSearch from '../../component/Dialogs/DialogSearch';
+// import DialogSearch from '../../component/Dialogs/DialogSearch';
 import DialogTips from '../../component/Dialogs/DialogTips';
 import DialogUsers from '../../component/Dialogs/DialogUsers';
 import DialogForum from '../../component/Dialogs/DialogForum';
+// import icon from '%PUBLIC_URL%/LOGO-SOLO.ico'
 
-import { logout, queryData } from '../../utils/firebase';
+import { logout } from '../../utils/firebase';
 
 let actions = [
-  { icon: <SearchIcon />, name: 'Search' },
+  // { icon: <SearchIcon />, name: 'Search' },
   { icon: <TipsAndUpdatesIcon />, name: 'Tips' },
   { icon: <PersonIcon />, name: 'Users' },
   { icon: <ForumIcon />, name: 'Forum' },
@@ -31,30 +34,63 @@ const Alert = React.forwardRef(function Alert(props, ref) {
 });
 
 function Maps() {
+
   const session = localStorage.getItem('user') ?? ""
-  const center = useMemo(() => ({ lat: 6.24788328286821, lng: -75.560914441354 }), [])
-  const Markers = [{ lat: 6.24788328286821, lng: -75.560914441354 }, { lat: 44, lng: -80 }]
+  // const center = useMemo(() => ({ lat: 6.24788328286821, lng: -75.560914441354 }), [])
+  const [center, setCenter] = useState(null)
+  const Markers = [{ lat: 44, lng: -80 }]
+  const [position, setPosition] = useState(null)
   const [map, setMap] = useState(null)
+  const [local, setLocal] = useState(false);
   const [open, setOpen] = useState(false);
   const [reload, setReload] = useState(false);
-  const [openDialogSearch, setOpenDialogSearch] = useState(false)
   const [openDialogTips, setOpenDialogTips] = useState(false)
   const [openDialogUsers, setOpenDialogUsers] = useState(false)
   const [openDialogForum, setOpenDialogForum] = useState(false)
   const [openAlert, setOpenAlert] = useState(false);
   const [severity, setSeverity] = useState('');
   const [message, setMessage] = useState('');
-
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+  const [values, setValues] = useState({});
 
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_GOOGLEMAPS_APIKEY,
     libraries: ['places']
   })
 
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+
+  function success(pos) {
+    var crd = pos.coords;
+
+    console.log('Your current position is:');
+    console.log('Latitude : ' + crd.latitude);
+    console.log('Longitude: ' + crd.longitude);
+    console.log('More or less ' + crd.accuracy + ' meters.');
+    setCenter({
+      lat: crd.latitude,
+      lng: crd.longitude
+    })
+    setPosition({
+      lat: crd.latitude,
+      lng: crd.longitude
+    })
+    setLocal(true)
+  };
+
+  function error(err) {
+    console.warn('ERROR(' + err.code + '): ' + err.message);
+  };
+
   useEffect(() => {
     const getInformation = async () => {
+      const options = {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0
+      };
+      navigator.geolocation.getCurrentPosition(success, error, options)
       const found = actions.find(element => element.name === 'Logout');
       if (reload) {
         setReload(false)
@@ -62,15 +98,15 @@ function Maps() {
       if (session && !found) {
         actions = [
           { icon: <PersonIcon />, name: 'Profile' },
-          { icon: <SearchIcon />, name: 'Search' },
+          // { icon: <SearchIcon />, name: 'Search' },
           { icon: <TipsAndUpdatesIcon />, name: 'Tips' },
           { icon: <ForumIcon />, name: 'Forum' },
           { icon: <LogoutIcon />, name: 'Logout' }
         ]
-        const dataUsers = await queryData('users')
-        const users = dataUsers.docs
-        const user = users.find(doc => doc?.data().uid === session)?.data()
-        console.log(user)
+        // const dataUsers = await queryData('users')
+        // const users = dataUsers.docs
+        // const user = users.find(doc => doc?.data().uid === session)?.data()
+        // console.log(user)
       }
     }
     getInformation()
@@ -92,9 +128,6 @@ function Maps() {
 
   const handleOption = (action) => {
     switch (action) {
-      case 'Search':
-        setOpenDialogSearch(true)
-        break;
       case 'Tips':
         setOpenDialogTips(true)
         break;
@@ -115,30 +148,38 @@ function Maps() {
   }
 
   const handleCloseDialog = () => {
-    setOpenDialogSearch(false)
     setOpenDialogTips(false)
     setOpenDialogUsers(false)
     setOpenDialogForum(false)
+    setValues({})
     handleClose()
-    setReload(true)
+    // setReload(true)
+  }
+
+  const handleSelect = (value) => {
+    setLocal(false)
+    if (value) {
+      const url = `https://maps.googleapis.com/maps/api/geocode/json?place_id=${value.value.place_id}&key=${process.env.REACT_APP_GOOGLEMAPS_APIKEY}`
+      axios.get(url)
+        .then((response) => {
+          // successLocation(response.data['results'][0].geometry.location)
+          // console.log('response.data[results][0].geometry.location', response.data['results'][0].geometry['location'])
+          setCenter(response.data['results'][0].geometry.location)
+        })
+        .catch((error) => {
+          console.log(error);
+        })
+    }
   }
 
   if (!isLoaded) return <div>Loading...</div>
-
-  const onPlaceChanged = () => {
-    if (this.autocomplete !== null) {
-      console.log(this.autocomplete.getPlace())
-    } else {
-      console.log('Autocomplete is not loaded yet!')
-    }
-  }
 
   return (
     <>
       <Box sx={{ transform: 'translateZ(0px)', flexGrow: 1, height: '100vh' }}>
         <Box sx={{ zIndex: 'modal' }}>
           <GoogleMap
-            zoom={15}
+            zoom={17}
             center={center}
             mapContainerStyle={{ width: '100%', height: '100vh' }}
             options={{
@@ -175,23 +216,31 @@ function Maps() {
                   />
                 ))}
               </SpeedDial>
-              <Card sx={{ marginLeft: 8, padding: 1, minWidth: 100 }} elevation={3}>
+              <Box sx={{ marginLeft: 8, padding: 1, minWidth: 100 }}>
                 <Stack direction={'row'} spacing={1}>
-                  <Autocomplete onPlaceChanged={onPlaceChanged}>
-                    <TextField
-                      fullWidth
-                      label={'Ubicacion'}
-                      size={'small'}
-                      variant="outlined"
-                    // onChange={(e) => console.log('coord', e.target.value)}
+                  <Box sx={{ width: '250px', fontFamily: 'monospace', fontSize: 15 }}>
+                    <GooglePlacesAutocomplete
+                      apiKey={process.env.REACT_APP_GOOGLEMAPS_APIKEY ?? ""}
+                      selectProps={{
+                        values,
+                        isClearable: true,
+                        onChange: (value) => {
+                          handleSelect(value)
+                          // console.log('value', value)
+                        },
+                        placeholder: 'Buscar...'
+                      }}
+                      onLoadFailed={(error) => (
+                        console.error("Could not inject Google script", error)
+                      )}
                     />
-                  </Autocomplete>
-                  <IconButton aria-label="delete" size="medium" color={'inherit'} onClick={() => map.panTo(center)}>
-                    <MyLocationIcon fontSize="inherit" />
-                  </IconButton>
+                  </Box>
                 </Stack>
-              </Card>
+              </Box>
             </Box>
+            {local && (
+              <Marker position={position} icon={'/pin.png'} onClick={() => console.log('esta es su ubicacion')} />
+            )}
             {Markers.map((marker, key) => {
               return (
                 <Marker key={key} position={marker} onClick={() => console.log(marker.lat, marker.lng)} />
@@ -205,9 +254,19 @@ function Maps() {
           left: 20,
           zIndex: 1,
         }}>
-          <DialogSearch open={openDialogSearch} onClose={handleCloseDialog} setReload={setReload} />
+          {/* <DialogSearch open={openDialogSearch} onClose={handleCloseDialog} setReload={setReload} /> */}
           <DialogTips open={openDialogTips} onClose={handleCloseDialog} setReload={setReload} />
           <DialogUsers open={openDialogUsers} onClose={handleCloseDialog} setReload={setReload} handleClickAlert={handleClickAlert} />
+          <Card sx={{ marginTop: '85vh', borderRadius: 20 }}>
+            <Stack>
+              {/* <IconButton aria-label="navigate" size="large" color={'inherit'}>
+                <NavigationIcon fontSize="inherit" />
+              </IconButton> */}
+              <IconButton aria-label="location" size="large" color={'inherit'} onClick={() => local ? map.panTo(center) : setReload(true)}>
+                <MyLocationIcon fontSize="inherit" />
+              </IconButton>
+            </Stack>
+          </Card>
           <DialogForum open={openDialogForum} onClose={handleCloseDialog} setReload={setReload} />
         </Box>
         <Snackbar open={openAlert} autoHideDuration={2000} onClose={handleCloseAlert}>
